ca54dbe2b247632c18410c8e899c0f8463a5175f
jasenmichael <jasen@ready-builder.com>
2018-10-06 21:55:18 -0400

    fixed working_dir

--- majaq.sh
+++ majaq.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 version="1.0"
 
-cd src
+# working_dir=~/majaq.io/src
+working_dir=`dirname $0`
+# echo $working_dir
 install () {
     echo "installing...."
+    rm -rf $working_dir/src/backend
+    git clone git@github.com:Majaq-io/majaq-dev-backend.git $working_dir/src/backend
 }
 
 start () {
     echo "Majaq version $version"
     echo "Starting...."
-    docker-compose up -d
-    # sudo chown -R $USER src/backend
-    if [ -f "src/files/wp-config.php" ]
+    cd $working_dir/src
+    docker-compose -f $working_dir/src/docker-compose.yml up -d
+    if [ -f "$working_dir/src/files/wp-config.php" ]
     then
-        rsync -s files/wp-config.php backend/wp-config.php
+        rsync -s $working_dir/src/files/wp-config.php $working_dir/src/backend/wp-config.php
     fi
 
-    if [ -d "src/files/wp-content" ]
+    if [ -d "$working_dir/src/files/wp-content" ]
     then
+        cd $working_dir/src
         docker-compose run wordpress rm -rf /var/www/html/wp-content
-        rsync -a files/wp-content/ backend/wp-content/
+        rsync -a $working_dir/src/files/wp-content/ $working_dir/src/backend/wp-content/
     # else
-        # cp -r src/backend/wp-content/ src/files
+        # cp -r $working_dir/src/backend/wp-content/ $working_dir/src/files
     fi
     isRunning
 }
@@ -29,9 +34,10 @@ start () {
 stop () {
     echo "Stopping...."
     # cd src
+    cd $working_dir/src
     docker-compose down
-    rsync -a backend/wp-content/ files/wp-content/
-    rsync -a backend/wp-config.php files/wp-config.php
+    rsync -a $working_dir/src/backend/wp-content/ $working_dir/src/files/wp-content/
+    rsync -a $working_dir/src/backend/wp-config.php $working_dir/src/files/wp-config.php
     isRunning
 }
 
@@ -67,53 +73,38 @@ Report bugs to: dev-team@majaq.io
 EOF
 }
 
-if [ $1 = "install" ]
+if [ "$1" = "install" ]
 then
     install
 fi
 
-if [ $1 = "start" ]
+if [ "$1" = "start" ]
 then
-    if [ $2 = "-s" ] && [ -z "$3" ]
+    if [ "$2" = "-s" ] && [ -z "$3" ]
     then
         SEED="default"
         seed
-    elif [ $2 = "-s" ] && [ $3 != "" ]
+    elif [ "$2" = "-s" ] && [ "$3" != "" ]
     then
-        SEED=$3
+        SEED="$3"
         seed
     fi
     # echo "$1 $2 $SEED"
     start
+    # sudo chown -R $USER $working_dir/src/backend
 fi
 
-if [ $1 = "stop" ]
+if [ "$1" = "stop" ]
 then
     stop
 fi
 
-if [ $1 = "-v" ]
+if [ "$1" = "-v" ]
 then
     echo $version
 fi
 
-if [ $1 = "-h" ]
+if [ "$1" = "-h" ]
 then
     usage
 fi
-
-
-
-
-
-
-# docker stop $(docker ps -a -q)
-# docker rm $(docker ps -a -q)
-# docker rmi $(docker images -q)
-# sudo chown -R $USER backend
-# docker-compose rm -f
-# docker-compose pull
-# docker-compose up --build -d
-# # Run some tests
-# ./tests
-# docker-compose stop -t 1
