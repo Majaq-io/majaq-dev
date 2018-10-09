191547ac66f3d76c8c0a83613ca9d85e45c950fa
jasenmichael <jasen@ready-builder.com>
2018-10-08 23:34:36 -0400

    add install backend

--- majaq.sh
+++ majaq.sh
@@ -102,7 +102,7 @@ checkUpdate () {
 
 updateBackend () {
     echo "Checking backend for updates"
-    if [ -d ./src/backend" ]
+    if [ -d "./src/backend" ]
     then
         installBackend
     else
