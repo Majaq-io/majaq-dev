ae93a8e0ffcc0e7b71eb28a0b3c3651445c4f9bb
jasenmichael <jasen@ready-builder.com>
2018-10-09 00:10:24 -0400

    add install backend

--- majaq.sh
+++ majaq.sh
@@ -10,8 +10,8 @@ installBackend () {
         echo "installing majaq-dev-backend in ./src/backend"
         rm -rf $working_dir/src/backend
         git clone git@github.com:Majaq-io/majaq-dev-backend.git $working_dir/src/backend
-        rsync -a $working_dir/src/backend/wp-content/ $working_dir/src/files/wp-content/
-        rsync -a $working_dir/src/backend/wp-config.php $working_dir/src/files/wp-config.php
+        rsync -a $working_dir/src/files/wp-content/ $working_dir/src/backend/wp-content/
+        rsync -a $working_dir/src/files/wp-config.php $working_dir/src/backend/wp-config.php
         exit
     fi
 }
