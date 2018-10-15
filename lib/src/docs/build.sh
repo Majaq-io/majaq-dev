87e9e9c6ae8165a330d88ff6f906984efd02eceb
jasenmichael <jasen@ready-builder.com>
2018-10-14 23:04:04 -0400

    add build script deploy to repo github page

---
+++ lib/src/docs/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# backup config.js to dev-config.js
+cp .vuepress/config.js .vuepress/dev-config.js
+
+# change base to majaq-dev
+sed -i -e 's/docs/majaq-dev/g' .vuepress/config.js
+
+# build it with new config
+vuepress build
+
+# put the dev config back
+cp .vuepress/dev-config.js .vuepress/config.js
+rm .vuepress/dev-config.js
+
+# remove root docs folder
+rm -rf ../../../docs
+
+# move the dist folder to root docs
+mv .vuepress/dist ../../../docs
