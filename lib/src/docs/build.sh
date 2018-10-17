847ef12145e30ae07b9cf53a6054dc2a7b520d46
jasenmichael <jasen@ready-builder.com>
2018-10-17 17:30:49 -0500

    tweak deploy script

--- lib/src/docs/build.sh
+++ lib/src/docs/build.sh
@@ -5,16 +5,19 @@ set -e
 
 # backup config.js to dev-config.js
 cp .vuepress/config.js .vuepress/dev-config.js
+sleep 1
 
 # change base to majaq-dev
 sed -i -e 's/docs/majaq-dev/g' .vuepress/config.js
-
+sleep 1
 # build it with new config
 vuepress build
 
 # put the dev config back
-rm .vuepress/config.js
+rm .vuepress/config.js 
+sleep 1
 cp .vuepress/dev-config.js .vuepress/config.js
+sleep 1
 rm .vuepress/dev-config.js
 
 # remove root docs folder
